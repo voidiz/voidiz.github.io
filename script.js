21c42ed7d7aa8a93a9cdd1c50bb1455696a8fcaf
voidiz <voidqt@hotmail.com>
2019-10-31 03:01:27 +0100

    finish basic rain

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ const VOLUME_TICK = 0.05;
 const MAX_VOLUME = 1;
 const MIN_VOLUME = 0;
 const INITIAL_VOLUME = 0.5;
-
-const PLAY_BUTTON = document.querySelector('button');
-const VOLUME_IND = document.querySelector('#volume');
 const AUDIO_ELEM = document.querySelector('audio');
 
-var width = window.innerWidth;
-var height = window.innerHeight;
+const MAX_DROPS = 200;
 
 var audioContext;
 var source;
 var gainNode;
 var analyzer;
-var currentVolume;
 var bufferLength;
 var dataArray;
+var currentVolume;
+var playing = false;
 
-var drawCounter = 0;
-var angle = 0;
+var img;
+var imgRatio = 16 / 9;
+var imgWidth;
+var imgHeight;
+
+var drops = [];
 
 function initAudio() {
   audioContext = new AudioContext();
@@ -31,7 +32,7 @@ function initAudio() {
   gainNode = audioContext.createGain();
   analyzer = audioContext.createAnalyser();
 
-  VOLUME_IND.textContent = gainNode.gain.value = INITIAL_VOLUME;
+  currentVolume = gainNode.gain.value = INITIAL_VOLUME;
   source.connect(analyzer);
   analyzer.connect(gainNode);
   gainNode.connect(audioContext.destination);
@@ -44,62 +45,131 @@ function initAudio() {
 function adjustVolume(delta) {
   currentVolume = Math.round(gainNode.gain.value * 100) / 100;
 
-  if ((currentVolume == MAX_VOLUME && delta > 0) || 
+  if ((currentVolume == MAX_VOLUME && delta > 0) ||
     (currentVolume == MIN_VOLUME && delta < 0)) {
     return;
   }
 
   gainNode.gain.value += delta;
   currentVolume = Math.round(gainNode.gain.value * 100) / 100;
-  VOLUME_IND.textContent = currentVolume;
 }
 
-function setup() {
-  // Listen for play/pause button clicks
-  PLAY_BUTTON.addEventListener('click', function() {
-    if (audioContext.state === 'suspended') {
-      audioContext.resume();
-    } 
+function mouseClicked() {
+  if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
 
-    if (this.dataset.playing === 'false') {
-      AUDIO_ELEM.play();
-      this.dataset.playing = 'true';
-    } else if (this.dataset.playing === 'true') {
-      AUDIO_ELEM.pause();
-      this.dataset.playing = 'false';
-    }
-  }, false);
+  if (playing === false) {
+    AUDIO_ELEM.play();
+    playing = true;
+  } else {
+    AUDIO_ELEM.pause();
+    playing = false;
+  }
+}
 
-  // Listen for scroll
-  window.addEventListener('wheel', function(e) {
-    var delta = -VOLUME_TICK * Math.sign(e.deltaY);
-    adjustVolume(delta);
-  }, false);
-
-  // Set playing attribute to false if song ends
-  AUDIO_ELEM.addEventListener('ended', () => {
-    PLAY_BUTTON.dataset.playing = 'false'; 
-  }, false);
-
-  initAudio();
-  createCanvas(windowWidth, windowHeight);
+function mouseWheel(e) {
+  var delta = -VOLUME_TICK * Math.sign(e.deltaY);
+  adjustVolume(delta);
 }
 
 function windowResized() {
+  calcImageDimensions();
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function draw_removethisafter() {
-  background(0);
-  translate(windowWidth / 2, windowHeight / 2);
-  rectMode(CENTER);
-  rotate(angle);
-  fill(0);
-  stroke(255, 0, 0);
+function calcImageDimensions() {
+  imgWidth = windowHeight * imgRatio;
+  imgHeight = imgWidth / imgRatio;
+
+  if (imgWidth < windowWidth) {
+    imgWidth = windowWidth;
+    imgHeight = imgWidth / imgRatio;
+  }
+}
+
+class Drop {
+  constructor() {
+    this.x = random(-windowWidth / 3, windowWidth);
+    this.y = random(-10, -5);
+    this.xs = windowWidth * 0.005;
+    this.ys = random(windowHeight * 0.01, windowHeight * 0.02);
+    this.len = random(windowHeight * 0.02, windowHeight * 0.04);
+    this.opacity = 0;
+    this.color = color(168, 221, 247);
+  }
+
+  fall() {
+    this.x += this.xs;
+    this.y += this.ys;
+
+    if (this.x > windowWidth || this.y > windowHeight) {
+      this.x = random(-windowWidth / 3, windowWidth);
+      this.y = random(-10, -5);
+      this.xs = windowWidth * 0.005;
+      this.ys = random(windowHeight * 0.01, windowHeight * 0.02);
+    }
+  }
+
+  show() {
+    var x2 = this.x + this.xs / this.ys * this.len;
+
+    this.color.setAlpha(this.opacity);
+    strokeWeight(2);
+    stroke(this.color);
+    line(this.x, this.y, x2, this.y + this.len);
+  }
+
+  fadeIn() {
+    if (this.opacity < 255) {
+      this.opacity += 5;
+    }
+  }
+
+  fadeOut() {
+    if (this.opacity > 0) {
+      this.opacity -= 5;
+    }
+  }
+}
+
+function preload() {
+  img = loadImage('bg.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  calcImageDimensions();
+  img.loadPixels();
+
+  for (var i = 0; i < MAX_DROPS; i++) {
+    drops[i] = new Drop();
+  }
+
+  // Set playing attribute to false if song ends
+  AUDIO_ELEM.addEventListener('ended', () => {
+    playing = false;
+  }, false);
+
+  initAudio();
+}
+
+function draw() {
+  image(img, 0, 0, imgWidth, imgHeight);
 
   analyzer.getByteFrequencyData(dataArray);
-  var mult = (windowWidth / 4) * dataArray[0]/255
-  rect(0, 0, mult, mult);
-  angle += 0.02;
-  drawCounter++;
+  var mult = (windowWidth / 4) * dataArray[0] / 255
+
+  var dropAmount = currentVolume / MAX_VOLUME * MAX_DROPS * playing;
+
+  for (var i = 0; i < MAX_DROPS; i++) {
+    drops[i].fall();
+    drops[i].show();
+    
+    if (i < dropAmount) {
+      drops[i].fadeIn();
+    } else {
+      drops[i].fadeOut();
+    }
+  }
 }
